7c038f5e5edc118f71e43dc89db48af69e559fb3
mercuryfire <mercuryfire.co@gmail.com>
2023-12-05 13:42:33 +0800

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,136 @@
+<template>
+  <q-page class="row q-pt-xl">
+    <div class="full-width q-px-xl">
+      <div class="q-mb-xl">
+        <q-input v-model="tempData.name" label="姓名" />
+        <q-input v-model="tempData.age" label="年齡" />
+      </div>
+
+      <q-table
+        flat
+        bordered
+        ref="tableRef"
+        :rows="blockData"
+        :columns="tableConfig"
+        row-key="id"
+        hide-pagination
+        separator="cell"
+      >
+        <template v-slot:header="props">
+          <q-tr :props="props">
+            <q-th v-for="col in props.cols" :key="col.name" :props="props">
+              {{ col.label }}
+            </q-th>
+            <q-th></q-th>
+          </q-tr>
+        </template>
+
+        <template v-slot:body="props">
+          <q-tr :props="props">
+            <q-td
+              v-for="col in props.cols"
+              :key="col.name"
+              :props="props"
+              style="min-width: 120px"
+            >
+              <div>{{ col.value }}</div>
+            </q-td>
+            <q-td class="text-right" auto-width v-if="tableButtons.length > 0">
+              <q-btn
+                @click="handleClickOption(btn, props.row)"
+                v-for="(btn, index) in tableButtons"
+                :key="index"
+                size="sm"
+                color="grey-6"
+                round
+                dense
+                :icon="btn.icon"
+                class="q-ml-md"
+                padding="5px 5px"
+              >
+                <q-tooltip
+                  transition-show="scale"
+                  transition-hide="scale"
+                  anchor="top middle"
+                  self="bottom middle"
+                  :offset="[10, 10]"
+                >
+                  {{ btn.label }}
+                </q-tooltip>
+              </q-btn>
+            </q-td>
+          </q-tr>
+        </template>
+        <template v-slot:no-data="{ icon }">
+          <div
+            class="full-width row flex-center items-center text-primary q-gutter-sm"
+            style="font-size: 18px"
+          >
+            <q-icon size="2em" :name="icon" />
+            <span> 無相關資料 </span>
+          </div>
+        </template>
+      </q-table>
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+interface btnType {
+  label: string;
+  icon: string;
+  status: string;
+}
+const blockData = ref([
+  {
+    name: 'test',
+    age: 25,
+  },
+]);
+const tableConfig = ref([
+  {
+    label: '姓名',
+    name: 'name',
+    field: 'name',
+    align: 'left',
+  },
+  {
+    label: '年齡',
+    name: 'age',
+    field: 'age',
+    align: 'left',
+  },
+]);
+const tableButtons = ref([
+  {
+    label: '編輯',
+    icon: 'edit',
+    status: 'edit',
+  },
+  {
+    label: '刪除',
+    icon: 'delete',
+    status: 'delete',
+  },
+]);
+
+const tempData = ref({
+  name: '',
+  age: '',
+});
+function handleClickOption(btn, data) {
+  // ...
+}
+</script>
+
+<style lang="scss" scoped>
+.q-table th {
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.q-table tbody td {
+  font-size: 18px;
+}
+</style>
