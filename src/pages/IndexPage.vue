85e16f9e902f00531dadc5bf8a3b2416fadb4e28
zu-chen <a2468929100@gmail.com>
2024-03-08 15:28:58 +0800

    CRUD

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,21 +3,12 @@
     <div class="full-width q-px-xl">
       <div class="q-mb-xl">
         <q-input v-model="tempData.name" label="姓名" />
-        <q-input v-model="tempData.age" label="年齡" />
-        <q-btn color="primary" class="q-mt-md">新增</q-btn>
+        <q-input v-model.number="tempData.age" label="年齡" />
+        <q-btn color="primary" class="q-mt-md" @click="handleCreate()">新增</q-btn>
       </div>
 
-      <q-table
-        flat
-        bordered
-        ref="tableRef"
-        :rows="blockData"
-        :columns="(tableConfig as QTableProps['columns'])"
-        row-key="id"
-        hide-pagination
-        separator="cell"
-        :rows-per-page-options="[0]"
-      >
+      <q-table flat bordered ref="tableRef" :rows="blockData" :columns="(tableConfig as QTableProps['columns'])"
+        row-key="id" hide-pagination separator="cell" :rows-per-page-options="[0]">
         <template v-slot:header="props">
           <q-tr :props="props">
             <q-th v-for="col in props.cols" :key="col.name" :props="props">
@@ -29,45 +20,23 @@
 
         <template v-slot:body="props">
           <q-tr :props="props">
-            <q-td
-              v-for="col in props.cols"
-              :key="col.name"
-              :props="props"
-              style="min-width: 120px"
-            >
+            <q-td v-for="col in props.cols" :key="col.name" :props="props" style="min-width: 120px">
               <div>{{ col.value }}</div>
             </q-td>
             <q-td class="text-right" auto-width v-if="tableButtons.length > 0">
-              <q-btn
-                @click="handleClickOption(btn, props.row)"
-                v-for="(btn, index) in tableButtons"
-                :key="index"
-                size="sm"
-                color="grey-6"
-                round
-                dense
-                :icon="btn.icon"
-                class="q-ml-md"
-                padding="5px 5px"
-              >
-                <q-tooltip
-                  transition-show="scale"
-                  transition-hide="scale"
-                  anchor="top middle"
-                  self="bottom middle"
-                  :offset="[10, 10]"
-                >
+              <q-btn @click="handleClickOption(btn, props.row)" v-for="(btn, index) in tableButtons" :key="index"
+                size="sm" color="grey-6" round dense :icon="btn.icon" class="q-ml-md" padding="5px 5px">
+                <q-tooltip transition-show="scale" transition-hide="scale" anchor="top middle" self="bottom middle"
+                  :offset="[10, 10]">
                   {{ btn.label }}
                 </q-tooltip>
               </q-btn>
             </q-td>
           </q-tr>
         </template>
+
         <template v-slot:no-data="{ icon }">
-          <div
-            class="full-width row flex-center items-center text-primary q-gutter-sm"
-            style="font-size: 18px"
-          >
+          <div class="full-width row flex-center items-center text-primary q-gutter-sm" style="font-size: 18px">
             <q-icon size="2em" :name="icon" />
             <span> 無相關資料 </span>
           </div>
@@ -123,9 +92,65 @@ const tempData = ref({
   name: '',
   age: '',
 });
+
 function handleClickOption(btn, data) {
-  // ...
+  console.log('btn', btn.status);
+  console.log('data', data);
+  data.age = 100;
+  if (btn.status === 'edit') {
+    const defaultName = data.name || '';
+    const defaultAge = data.age || '';
+    const newName = prompt('Enter new name:', defaultName);
+    const newAge = prompt('Enter new age:', defaultAge);
+
+    if (newName !== null) {
+      data.name = newName;
+    }
+
+    if (newAge !== null) {
+      data.age = Number(newAge);
+    }
+    axios.patch('https://demo.mercuryfire.com.tw:49110/crudTest', data)
+      .then(res => {
+        handleGet();
+      })
+      .catch(err => {
+        console.log('err', err);
+      })
+  } else {
+    // console.log('data', data.id);
+    // return
+    axios.delete(`https://demo.mercuryfire.com.tw:49110/crudTest/${data.id}`)
+      .then(res => {
+        handleGet();
+      })
+      .catch(err => {
+        console.log('err', err);
+      })
+  }
 }
+function handleGet() {
+  axios.get('https://demo.mercuryfire.com.tw:49110/crudTest/a').then((response) => {
+    // console.log(response.data)
+    blockData.value = response.data.result;
+  })
+}
+function handleCreate() {
+  axios.post('https://demo.mercuryfire.com.tw:49110/crudTest', tempData.value, {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(response => {
+      console.log(response);
+      handleGet();
+    })
+    .catch(error => {
+      console.error('Error creating:', error);
+    });
+}
+
+handleGet();
 </script>
 
 <style lang="scss" scoped>
